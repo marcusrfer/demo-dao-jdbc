7734ef6f5ce2a7909c9906e519f2ed8237f1fd02
Marcus Rangel <marcus.fer@gmail.com>
2023-12-13 02:15:28 -0300

    Seller entity class

--- AppDaoJdbc.java
+++ AppDaoJdbc.java
@@ -1,10 +1,16 @@
 package demo_dao_jdbc;
 
 import demo_dao_jdbc.entities.Department;
+import demo_dao_jdbc.entities.Seller;
+
+import java.util.Date;
 
 public class AppDaoJdbc {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
