1dc5a05d2dc9cc0d2475d701543b18c1d0229dba
Marcus Rangel <marcus.fer@gmail.com>
2023-12-17 01:29:18 -0300

    Implementação metodos DAO

--- dao/DaoFactory.java
+++ dao/DaoFactory.java
@@ -5,11 +5,11 @@ import demo_dao_jdbc.impl.DepartmentDaoJdbc;
 import demo_dao_jdbc.impl.SellerDaoJdbc;
 
 public class DaoFactory {
-    public static SellerDAO createSellerDao(){
+    public static SellerDAO createSellerDao() {
         return new SellerDaoJdbc(DB.getConnection());
     }
 
-    public static DepartmentDAO createDepartmentDao(){
+    public static DepartmentDAO createDepartmentDao() {
         return new DepartmentDaoJdbc(DB.getConnection());
     }
 }
